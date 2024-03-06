195092b9a30f93c339b558f3c7ecc131db9da3b2
Sanaa <ahdoursanaa@gmail.com>
2024-03-06 17:25:11 +0400

    Updates Nav

--- src/Components/Header/Navigation/Navigation.js
+++ src/Components/Header/Navigation/Navigation.js
@@ -3,48 +3,48 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 import "./navigation.css";
 
 function Navigation() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!mobileMenuOpen);
-    };
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
 
-    const closeMobileMenu = () => {
-        setMobileMenuOpen(false);
-    };
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
 
-    return (
-        <div className="navigation-container">
-            {/* Menu de navigation principal */}
-            <ul className={`Navigation ${mobileMenuOpen ? 'active' : ''}`}>
-                <NavigationItem to="/">Accueil</NavigationItem>
-                <NavigationItem to="/activites">Activités</NavigationItem>
-                <NavigationItem to="/anniversaires">Anniversaires</NavigationItem>
-                <NavigationItem to="/restaurant">Restaurant</NavigationItem>
-                <NavigationItem to="/groupes">Groupes</NavigationItem>
-                <NavigationItem to="/contact">Contact</NavigationItem>
-            </ul>
+  return (
+    <div className="navigation-container">
+      {/* Menu de navigation principal */}
+      <ul className={`Navigation ${mobileMenuOpen ? 'active' : ''}`}>
+        <NavigationItem to="/" onClick={closeMobileMenu}>Accueil</NavigationItem>
+        <NavigationItem to="/activites" onClick={closeMobileMenu}>Activités</NavigationItem>
+        <NavigationItem to="/anniversaires" onClick={closeMobileMenu}>Anniversaires</NavigationItem>
+        <NavigationItem to="/restaurant" onClick={closeMobileMenu}>Restaurant</NavigationItem>
+        <NavigationItem to="/groupes" onClick={closeMobileMenu}>Groupes</NavigationItem>
+        <NavigationItem to="/contact" onClick={closeMobileMenu}>Contact</NavigationItem>
+      </ul>
 
-            {/* Bouton de menu déroulant pour la version mobile */}
-            <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
-                {mobileMenuOpen ? '✕' : '☰'}
-            </div>
+      {/* Bouton de menu déroulant pour la version mobile */}
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        {mobileMenuOpen ? '✕' : '☰'}
+      </div>
 
-            {/* Menu déroulant pour la version mobile */}
-            <div className={`MobileNavigation ${mobileMenuOpen ? 'active' : ''}`}>
-                {/* Éléments de navigation pour le menu mobile */}
-                <div className="fermer-menu" onClick={closeMobileMenu}>
-                    ✕ 
-                </div>
-                <NavigationItem to="/" onClick={closeMobileMenu}>Accueil</NavigationItem>
-                <NavigationItem to="/activites" onClick={closeMobileMenu}>Activités</NavigationItem>
-                <NavigationItem to="/anniversaires" onClick={closeMobileMenu}>Anniversaires</NavigationItem>
-                <NavigationItem to="/restaurant" onClick={closeMobileMenu}>Restaurant</NavigationItem>
-                <NavigationItem to="/groupes" onClick={closeMobileMenu}>Groupes</NavigationItem>
-                <NavigationItem to="/contact" onClick={closeMobileMenu}>Contact</NavigationItem>
-            </div>
+      {/* Menu déroulant pour la version mobile */}
+      <div className={`MobileNavigation ${mobileMenuOpen ? 'active' : ''}`}>
+        {/* Éléments de navigation pour le menu mobile */}
+        <div className="fermer-menu" onClick={closeMobileMenu}>
+          ✕
         </div>
-    );
+        <NavigationItem to="/" onClick={closeMobileMenu}>Accueil</NavigationItem>
+        <NavigationItem to="/activites" onClick={closeMobileMenu}>Activités</NavigationItem>
+        <NavigationItem to="/anniversaires" onClick={closeMobileMenu}>Anniversaires</NavigationItem>
+        <NavigationItem to="/restaurant" onClick={closeMobileMenu}>Restaurant</NavigationItem>
+        <NavigationItem to="/groupes" onClick={closeMobileMenu}>Groupes</NavigationItem>
+        <NavigationItem to="/contact" onClick={closeMobileMenu}>Contact</NavigationItem>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
--- src/Components/VideoBanner/VideoBanner.js
+++ src/Components/VideoBanner/VideoBanner.js
@@ -18,7 +18,7 @@ const VideoBanner = ({ mobileVideo, desktopVideo, alt }) => {
 
   return (
     <div className="VideoBanner">
-      <video autoPlay loop muted>
+      <video autoPlay loop >
         <source src={isMobile ? mobileVideo : desktopVideo} type="video/mp4" />
         Votre navigateur ne prend pas en charge la balise vidéo.
       </video>
